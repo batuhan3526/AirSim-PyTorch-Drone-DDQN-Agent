7ee0e8fe14dbcf22ccd7b8b7bba8c7edc322a65e
Batuhan Bayraktar <52297554+batuhan3526@users.noreply.github.com>
2021-03-08 15:57:30 +0300

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from agent import Agent
-from ddqn_agent import DDQN_Agent
-
-if __name__ == "__main__":
-    #agent = Agent(useGPU=True, useDepth=True)
-    #agent.train()
-    ddqn_agent = DDQN_Agent(useDepth=False)
-    ddqn_agent.train()
